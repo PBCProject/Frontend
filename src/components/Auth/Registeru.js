182fbf93aca75d23cd9394e34cbeb880cff1f12b
jgoper <jgoper@gmail.com>
2021-12-02 18:47:47 -0500

    Formulario de registro
    Se colocaron los datos necesarios para el registro de los clientes.

--- src/components/Auth/Registeru.js
+++ src/components/Auth/Registeru.js
@@ -12,6 +12,38 @@ export function Registeru(){
                     </div>
                     <div className="modal-body">
                         <form>
+                        <div className="form-group">
+                                <label htmlFor="exampleInputPassword1">Nombre Completo</label>
+                                <input type="text" className="form-control" id="exampleInputPassword1" placeholder="Nombre" />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="exampleInputPassword1">Fecha de Nacimiento</label>
+                                <input type="date" className="form-control" id="exampleInputPassword1" placeholder="Fecha de Nacimiento" />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="exampleInputPassword1">Tipo de Documento</label>
+                                <select className="form-control">
+                                    <option>C.C.</option>
+                                    <option>T.E.</option>   
+                                    <option>NIT</option>   
+                                </select>
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="exampleInputPassword1">Numero de Documento</label>
+                                <input type="number" className="form-control" id="exampleInputPassword1" placeholder="Numero de Documento" />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="exampleInputPassword1">Fecha de Espedición del Documento</label>
+                                <input type="date" className="form-control" id="exampleInputPassword1" placeholder="Fecha de Nacimiento" />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="exampleInputPassword1">Valor de Ingresos</label>
+                                <input type="number" className="form-control" id="exampleInputPassword1" placeholder="Valor de Ingresos" />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="exampleInputPassword1">Valor de Egresos</label>
+                                <input type="number" className="form-control" id="exampleInputPassword1" placeholder="Valor de Egresos" />
+                            </div>
                             <div className="form-group">
                                 <label htmlFor="exampleInputEmail1">Email</label>
                                 <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
@@ -25,46 +57,11 @@ export function Registeru(){
                                 <label htmlFor="exampleInputPassword1">Confirmar Password</label>
                                 <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
                             </div>
-                            <div className="form-group">
-                                <label htmlFor="exampleInputPassword1">Nombre</label>
-                                <input type="text" className="form-control" id="exampleInputPassword1" placeholder="Nombre" />
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="exampleInputPassword1">Apellido</label>
-                                <input type="text" className="form-control" id="exampleInputPassword1" placeholder="Apellido" />
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="exampleInputPassword1">Direccion</label>
-                                <input type="text" className="form-control" id="exampleInputPassword1" placeholder="Direccion" />
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="exampleInputPassword1">Telefono</label>
-                                <input type="text" className="form-control" id="exampleInputPassword1" placeholder="Telefono" />
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="exampleInputPassword1">Fecha de Nacimiento</label>
-                                <input type="date" className="form-control" id="exampleInputPassword1" placeholder="Fecha de Nacimiento" />
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="exampleInputPassword1">Genero</label>
-                                <select className="form-control">
-                                    <option>Masculino</option>
-                                    <option>Femenino</option>
-                                </select>
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="exampleInputPassword1">Tipo de Usuario</label>
-                                <select className="form-control">
-                                    <option>Administrador</option>
-                                    <option>Funcionario</option>   
-                                    <option>Usuario</option>   
-                                </select>
-                            </div>
                         </form>
                     </div>
                     <div className="modal-footer">
                         <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-                        <button type="button" className="btn btn-primary">Registrarse</button>
+                        <button type="button" className="btn btn-secondary">Registrarse</button>
                     </div>
                 </div>
             </div>
